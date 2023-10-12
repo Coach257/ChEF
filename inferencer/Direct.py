2da7680c80e95eb1ed0496221ae6803345f45210
Coach257 <shizhelun20000820@126.com>
2023-10-12 22:02:02 +0800

    support detdirect+ icl_direct

--- inferencer/Direct.py
+++ inferencer/Direct.py
@@ -49,3 +49,67 @@ class Direct_inferencer:
         with open(answer_path, "w") as f:
             f.write(json.dumps(predictions, indent=4))
         self.results_path = answer_path
+
+
+
+class Det_Direct_inferencer(Direct_inferencer):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        from metric.utils import classification_acc, Cleaner
+        self.cleaner = Cleaner()
+        self.check_label = classification_acc
+
+    def inference(self, model, dataset):
+        classification_prompt = self.instruction_handler.query[0]
+        bbox_prompt = self.instruction_handler.query[1]
+        predictions=[]
+        dataloader = DataLoader(dataset, batch_size=self.batch_size, collate_fn=lambda batch: {key: [dict[key] for dict in batch] for key in batch[0]})
+
+        for batch in tqdm(dataloader, desc="Running inference"):
+            outputs = model.batch_generate(batch['image_path'], [classification_prompt for _ in range(len(batch['image_path']))],max_new_tokens=self.max_new_tokens)
+
+            res_dict_list = [dict() for i in range(len(batch['image_path']))]
+            for idx, (gt_objects, pred_text) in enumerate(zip(batch['gt_answers'], outputs)):
+                for gt_object in gt_objects:
+                    if gt_object['label'] in res_dict_list[idx]:
+                        continue
+                    res_dict_list[idx][gt_object['label']] = None
+                    if self.check_label(gt_object['label'], self.cleaner.clean(pred_text)): # filter correct classification answers
+                        pred_bbox = model.generate(batch['image_path'][idx], bbox_prompt.format(gt_object['label']), max_new_tokens = self.max_new_tokens)
+                        res_dict_list[idx][gt_object['label']] = pred_bbox
+            for i in range(len(res_dict_list)):
+                answer_dict = copy_batch_dict(batch, i)
+                answer_dict['question'] = classification_prompt + '\n' + bbox_prompt
+                answer_dict['answer'] = res_dict_list[i]
+                answer_dict['classification_answer'] = outputs[i]
+                predictions.append(answer_dict)
+
+        self._after_inference_step(predictions)
+
+
+class Icl_Direct_inferencer(Direct_inferencer):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+    def inference(self, model, dataset):
+        predictions=[] 
+        dataloader = DataLoader(dataset, batch_size=self.batch_size, collate_fn=lambda batch: {key: [dict[key] for dict in batch] for key in batch[0]})
+
+        for batch_idx, batch in enumerate(tqdm(dataloader, desc="Running inference")):
+            if self.CoT:
+                prompts, cot = self.instruction_handler.generate_CoT_query(model, batch)
+            else:
+                prompts = self.instruction_handler.generate_basic_query(batch)
+
+            ices = self.instruction_handler.generate_ices(prompts, batch_idx, self.batch_size)
+
+            outputs, icl_prompts = model.icl_batch_generate(batch['image_path'], prompts, ices, self.instruction_handler.icl_cfg, max_new_tokens=self.max_new_tokens)
+            for i in range(len(outputs)):
+                answer_dict = copy_batch_dict(batch, i)
+                answer_dict['query'] = icl_prompts[i]
+                answer_dict['answer'] = outputs[i]
+                if self.CoT:
+                    answer_dict['CoT_answer'] = cot[i]
+
+                predictions.append(answer_dict)
+        self._after_inference_step(predictions)
--- inferencer/__init__.py
+++ inferencer/__init__.py
@@ -1,13 +1,13 @@
-from .Direct import Direct_inferencer
+from .Direct import Direct_inferencer, Det_Direct_inferencer, Icl_Direct_inferencer
 from .PPL import PPL_inferencer, ICL_PPL_inferencer, Det_PPL_inferencer
 
 inferencer_dict = {
     'Direct': Direct_inferencer,
-    # 'detection': Direct_inferencer,
+    'Det': Det_Direct_inferencer,
     'Det_PPL': Det_PPL_inferencer,
     'PPL': PPL_inferencer,
     # 'multi_ppl': Multi_Turn_PPL_inferencer,
-    # 'In_context_learning': Icl_inferencer,
+    'ICL_Direct': Icl_Direct_inferencer,
     # 'Calibration':Cali_inferencer,
     'ICL_PPL': ICL_PPL_inferencer
 }
